02eabd5ba4f8532ffc44588a816e41cf3f205931
amar-jay <64834413+amar-jay@users.noreply.github.com>
2023-05-27 14:59:51 +0300

    error page and vercel redirects

---
+++ demo/demo/src/pages/Error.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface ErrorProps {
+  message: string;
+}
+export const Error = (
+  { message }: ErrorProps = { message: "Page not found" }
+) => {
+  return (
+    <div className="flex items-center justify-center gap-3 flex-col">
+      <h1>Oops!!</h1>
+      <span className="read-the-docs">{message}</span>
+    </div>
+  );
+};
